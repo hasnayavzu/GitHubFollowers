d7e48696f745554556fd00cad8e1192b6d4632e9
hasnayavzu <hasan.yavuz@ozu.edu.tr>
2023-10-15 20:14:29 +0300

    First commit

---
+++ GitHubFollowers/FavoritesListsVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListsVC.swift
+//  GitHubFollowers
+//
+//  Created by Hasan Yavuz on 15.10.2023.
+//
+
+import UIKit
+
+class FavoritesListsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Hasan Yavuz on 15.10.2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
