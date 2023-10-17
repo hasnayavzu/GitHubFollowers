569eb69a7bb01a43b11b781249cdd5b129f8554f
hasnayavzu <hasan.yavuz@ozu.edu.tr>
2023-10-17 17:13:21 +0300

    created a custom alert and created a reusable components for later alerts we might use.

---
+++ GitHubFollowers/GFAlertCV.swift
@@ -0,0 +1,33 @@
+//
+//  GFAlertCV.swift
+//  GitHubFollowers
+//
+//  Created by Hasan Yavuz on 17.10.2023.
+//
+
+import UIKit
+
+class GFAlertCV: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
---
+++ GitHubFollowers/UIViewController+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Ext.swift
+//  GitHubFollowers
+//
+//  Created by Hasan Yavuz on 17.10.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+    
+}
