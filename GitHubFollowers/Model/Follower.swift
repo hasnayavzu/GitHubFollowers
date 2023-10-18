29ffb7e8cf39c6e7cc2175b1024576e41c86a556
hasnayavzu <hasan.yavuz@ozu.edu.tr>
2023-10-18 16:31:41 +0300

    created model files for network calls and fixed some bug about nav bar being hidden

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Hasan Yavuz on 18.10.2023.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
