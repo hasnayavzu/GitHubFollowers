d7a9fa79a25bacdaa33937db858b69aa11dc1b6d
hasnayavzu <hasan.yavuz@ozu.edu.tr>
2023-10-24 00:05:25 +0300

    created the cache system for avatar images

--- GitHubFollowers/Custom Views/GFAvatarImageView.swift
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
     let placeholderImage = UIImage(named: "avatar-placeholder")!
+    let cache = NetworkManager.shared.cache
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,6 +30,12 @@ class GFAvatarImageView: UIImageView {
     func downloadImage(from urlString: String) {
         guard let url = URL(string: urlString) else { return }
 
+        let cacheKey = NSString(string: urlString)
+        if let image = cache.object(forKey: cacheKey) {
+            self.image = image
+            return
+        }
+
         let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
             guard let self = self else { return }
 
@@ -38,9 +45,8 @@ class GFAvatarImageView: UIImageView {
 
             guard let image = UIImage(data: data) else { return }
 
-            DispatchQueue.main.async {
-                self.image = image
-            }
+            cache.setObject(image, forKey: cacheKey)
+            DispatchQueue.main.async { self.image = image }
         }
 
         task.resume()
