62852ad25aad71d1f65365d36852a1937e849151
hasnayavzu <hasan.yavuz@ozu.edu.tr>
2023-11-05 12:58:28 +0300

    created the userInfoVC and made the connections

---
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,25 @@
+//
+//  UserInfoVC.swift
+//  GitHubFollowers
+//
+//  Created by Hasan Yavuz on 5.11.2023.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+
+}
